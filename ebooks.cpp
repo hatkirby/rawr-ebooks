26d75f744913a8856e46f5fccbfda8f8336924a0
Kelly Rauchenberger <fefferburbia@gmail.com>
2018-08-06 22:51:15 -0400

    Switched from streaming API to timeline polling
    This means the bot is also now single-threaded.

--- ebooks.cpp
+++ ebooks.cpp
@@ -12,20 +12,23 @@
 #include <chrono>
 #include <algorithm>
 
+const auto QUEUE_TIMEOUT = std::chrono::minutes(1);
+const auto POLL_TIMEOUT = std::chrono::minutes(5);
+
 int main(int argc, char** args)
 {
   srand(time(NULL));
   rand(); rand(); rand(); rand();
-	
+
   YAML::Node config = YAML::LoadFile("config.yml");
   int delay = config["delay"].as<int>();
-  
-  twitter::auth auth;
-  auth.setConsumerKey(config["consumer_key"].as<std::string>());
-  auth.setConsumerSecret(config["consumer_secret"].as<std::string>());
-  auth.setAccessKey(config["access_key"].as<std::string>());
-  auth.setAccessSecret(config["access_secret"].as<std::string>());
-  
+
+  twitter::auth auth(
+    config["consumer_key"].as<std::string>(),
+    config["consumer_secret"].as<std::string>(),
+    config["access_key"].as<std::string>(),
+    config["access_secret"].as<std::string>());
+
   twitter::client client(auth);
 
   std::ifstream infile(config["corpus"].as<std::string>().c_str());
@@ -37,10 +40,10 @@ int main(int argc, char** args)
     {
       line.pop_back();
     }
-    
+
     corpus += line + "\n";
   }
-  
+
   // Replace old-style freevars while I can't be bothered to remake the corpus yet
   std::vector<std::string> fv_names;
   std::ifstream namefile("names.txt");
@@ -54,11 +57,11 @@ int main(int argc, char** args)
       {
         l.pop_back();
       }
-      
+
       fv_names.push_back(l);
     }
   }
-  
+
   namefile.close();
 
   std::cout << "Preprocessing corpus..." << std::endl;
@@ -71,60 +74,114 @@ int main(int argc, char** args)
     {
       form.replace(pos, 6, fv_names[rand() % fv_names.size()]);
     }
-    
+
     return form;
   });
-  
-  twitter::stream user_stream(client, [&kgramstats, &client] (const twitter::notification& n) {
-    if (n.getType() == twitter::notification::type::tweet)
-    {
-      if ((!n.getTweet().isRetweet()) && (n.getTweet().getAuthor() != client.getUser()))
-      {
-        std::string original = n.getTweet().getText();
-        std::string canonical;
-        std::transform(std::begin(original), std::end(original), std::back_inserter(canonical), [] (char ch) {
-          return std::tolower(ch);
-        });
-      
-        if (canonical.find("@rawr_ebooks") != std::string::npos)
-        {
-          std::string doc = n.getTweet().generateReplyPrefill(client.getUser());
-          doc += kgramstats.randomSentence(140 - doc.length());
-          doc.resize(140);
 
-          try
-          {
-            client.replyToTweet(doc, n.getTweet());
-          } catch (const twitter::twitter_error& error)
-          {
-            std::cout << "Twitter error while tweeting: " << error.what() << std::endl;
-          }
-        }
-      }
-    }
-  });
-  
-  std::this_thread::sleep_for(std::chrono::minutes(1));
+  std::list<std::tuple<std::string, bool, twitter::tweet_id>> postQueue;
 
-  std::cout << "Generating..." << std::endl;
+  auto startedTime = std::chrono::system_clock::now();
+
+  auto queueTimer = std::chrono::system_clock::now();
+  auto pollTimer = std::chrono::system_clock::now();
+  auto genTimer = std::chrono::system_clock::now();
+
   for (;;)
   {
-    std::string doc = kgramstats.randomSentence(140);
-    doc.resize(140);
-    
-    try
+    auto currentTime = std::chrono::system_clock::now();
+
+    if (currentTime >= genTimer)
     {
-      client.updateStatus(doc);
-    } catch (const twitter::twitter_error& error)
-    {
-      std::cout << "Twitter error while tweeting: " << error.what() << std::endl;
+      std::string doc = kgramstats.randomSentence(140);
+      doc.resize(140);
+
+      postQueue.emplace_back(std::move(doc), false, 0);
+
+      int genwait = rand() % delay + 1;
+
+      genTimer = currentTime + std::chrono::seconds(genwait);
     }
-    
-    int waitlen = rand() % delay;
-    if (waitlen == 0)
+
+    if (currentTime >= pollTimer)
     {
-      continue;
-    } else if (waitlen == 1)
+      pollTimer = currentTime;
+
+      try
+      {
+        std::list<twitter::tweet> newTweets =
+          client.getMentionsTimeline().poll();
+
+        for (const twitter::tweet& tweet : newTweets)
+        {
+          auto createdTime =
+            std::chrono::system_clock::from_time_t(tweet.getCreatedAt());
+
+          if (
+            // Ignore tweets from before the bot started up
+            createdTime > startedTime
+            // Ignore retweets
+            && !tweet.isRetweet()
+            // Ignore tweets from yourself
+            && tweet.getAuthor() != client.getUser())
+          {
+            std::string doc = tweet.generateReplyPrefill(client.getUser());
+            doc += kgramstats.randomSentence(140 - doc.length());
+            doc.resize(140);
+
+            postQueue.emplace_back(std::move(doc), true, tweet.getID());
+          }
+        }
+      } catch (const twitter::rate_limit_exceeded&)
+      {
+        // Wait out the rate limit (10 minutes here and 5 below = 15).
+        pollTimer += std::chrono::minutes(10);
+      } catch (const twitter::twitter_error& e)
+      {
+        std::cout << "Twitter error while polling: " << e.what() << std::endl;
+      }
+
+      pollTimer += std::chrono::minutes(POLL_TIMEOUT);
+    }
+
+    if ((currentTime >= queueTimer) && (!postQueue.empty()))
+    {
+      auto post = postQueue.front();
+      postQueue.pop_front();
+
+      try
+      {
+        if (std::get<1>(post))
+        {
+          client.replyToTweet(std::get<0>(post), std::get<2>(post));
+        } else {
+          client.updateStatus(std::get<0>(post));
+        }
+      } catch (const twitter::twitter_error& error)
+      {
+        std::cout << "Twitter error while tweeting: " << error.what()
+          << std::endl;
+      }
+
+      queueTimer = currentTime + std::chrono::minutes(QUEUE_TIMEOUT);
+    }
+
+    auto soonestTimer = genTimer;
+
+    if (pollTimer < soonestTimer)
+    {
+      soonestTimer = pollTimer;
+    }
+
+    if ((queueTimer < soonestTimer) && (!postQueue.empty()))
+    {
+      soonestTimer = queueTimer;
+    }
+
+    int waitlen =
+      std::chrono::duration_cast<std::chrono::seconds>(
+        soonestTimer - currentTime).count();
+
+    if (waitlen == 1)
     {
       std::cout << "Sleeping for 1 second..." << std::endl;
     } else if (waitlen < 60)
@@ -135,22 +192,25 @@ int main(int argc, char** args)
       std::cout << "Sleeping for 1 minute..." << std::endl;
     } else if (waitlen < 60*60)
     {
-      std::cout << "Sleeping for " << (waitlen/60) << " minutes..." << std::endl;
+      std::cout << "Sleeping for " << (waitlen/60) << " minutes..."
+        << std::endl;
     } else if (waitlen == 60*60)
     {
       std::cout << "Sleeping for 1 hour..." << std::endl;
     } else if (waitlen < 60*60*24)
     {
-      std::cout << "Sleeping for " << (waitlen/60/60) << " hours..." << std::endl;
+      std::cout << "Sleeping for " << (waitlen/60/60) << " hours..."
+        << std::endl;
     } else if (waitlen == 60*60*24)
     {
       std::cout << "Sleeping for 1 day..." << std::endl;
     } else {
-      std::cout << "Sleeping for " << (waitlen/60/60/24) << " days..." << std::endl;
+      std::cout << "Sleeping for " << (waitlen/60/60/24) << " days..."
+        << std::endl;
     }
 
-    std::this_thread::sleep_for(std::chrono::seconds(waitlen));
+    std::this_thread::sleep_until(soonestTimer);
   }
-	
+
   return 0;
 }
